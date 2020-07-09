fca15f0ee9f8aaadb3de826ef139f646529396b5
rishav-learnerml <rishavbio1@gmail.com>
2020-07-09 14:26:24 +0530

    Updates

---
+++ precache-manifest.aec98cc8b3b70c0c4dce9484a875799c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "570db385747c211be337351246ef5f54",
+    "url": "/User-Profile/index.html"
+  },
+  {
+    "revision": "72abdb4c435c9707eb26",
+    "url": "/User-Profile/static/css/main.f49676ba.chunk.css"
+  },
+  {
+    "revision": "e31a2ba08e21853dd792",
+    "url": "/User-Profile/static/js/2.375a3f5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Profile/static/js/2.375a3f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72abdb4c435c9707eb26",
+    "url": "/User-Profile/static/js/main.05726811.chunk.js"
+  },
+  {
+    "revision": "845db54e6f115d4fad07",
+    "url": "/User-Profile/static/js/runtime-main.62f800cf.js"
+  },
+  {
+    "revision": "f90bc74317b28114c7022820e4c2f251",
+    "url": "/User-Profile/static/media/newlogo.f90bc743.png"
+  }
+]);
